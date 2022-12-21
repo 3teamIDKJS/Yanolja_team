5bbc0d3d0feeb15c807b2ab2218395244b26517d
frdytheme <rbfridayy@gmail.com>
2022-12-21 11:11:20 +0900

    .

--- ESG/js/ESG.js
+++ ESG/js/ESG.js
@@ -1,1 +1,14 @@
-console.log(document.documentElement.clientHeight);
+// top버튼
+const topBtn = document.querySelector(".topBtn");
+const btnArrow = document.querySelector(".topBtn #downArrow");
+
+btnArrow.style.top = `-${btnArrow.offsetHeight}px`;
+const arrowFall = () => {
+  btnArrow.style.top = `${btnArrow.offsetHeight * 2}px`
+};
+
+setInterval(arrowFall, 2000);
+
+function changeBtn() {}
+
+addEventListener("scroll", changeBtn);
